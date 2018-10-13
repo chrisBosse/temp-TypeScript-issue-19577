7e335240f96ba4bf696c325664faaabb3950c890
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2018-10-12 20:35:28 -0400

    Trigger debugger
    https://github.com/Microsoft/TypeScript/issues/19577#issuecomment-407455706

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 node ../TypeScript/built/local/tsc.js --noImplicitAny 19577-workaround.ts
-node ../TypeScript/built/local/tsc.js --noImplicitAny 19577-bug-trigger.ts
+node --inspect-brk ../TypeScript/built/local/tsc.js --noImplicitAny 19577-bug-trigger.ts
