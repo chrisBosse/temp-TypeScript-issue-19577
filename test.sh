44ca426055d9d323107f92f20e1ef217331a7f8e
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2018-10-26 07:31:18 -0400

    Side-by-side testing

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-node ../TypeScript/built/local/tsc.js --noImplicitAny 19577-workaround.ts
-node --inspect-brk ../TypeScript/built/local/tsc.js --noImplicitAny 19577-bug-trigger.ts
+node --inspect-brk:9228 ../TypeScript/built/local/tsc.js --noImplicitAny 19577-workaround.ts &
+node --inspect-brk ../TypeScript/built/local/tsc.js --noImplicitAny 19577-bug-trigger.ts &
